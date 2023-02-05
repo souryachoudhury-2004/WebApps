e04a621e4a5743d83f9f3fba7e9b34f7cfab5b51
Sourya Choudhury <souryachoudhury24@gmail.com>
2023-02-05 15:28:00 +0530

    Pretty much a completed app that works.

--- main.py
+++ main.py
@@ -3,12 +3,26 @@ import fileHandler
 
 todos = fileHandler.getTodos()
 
+def add_todo():
+    todo = st.session_state["new_todo"]
+    todo = todo+"\n"
+    todos.append(todo)
+    fileHandler.writeTodos(todos)
+
+
 st.title("My Todo App")
 st.subheader("This is a todo list app.")
 st.write("This app is to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        del st.session_state[todo]
+        fileHandler.writeTodos(todos)
+        st.experimental_rerun()
 
-st.text_input(label = "", value="", placeholder="Add new todo...")
+st.text_input(label="", value="", placeholder="Add new todo...", on_change=add_todo, key="new_todo")
+
+
 
